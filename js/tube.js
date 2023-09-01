b5fc06161a08163c1e43972a45eac9126fa9add5
tahmidurrahmann <mirazurrahmannn@gmail.com>
2023-09-02 03:50:12 +0600

    added the min n hour part

--- js/tube.js
+++ js/tube.js
@@ -10,9 +10,9 @@ const clickFunction = (allCategory) => {
     allCategory.forEach((category) => {
         const div = document.createElement('div');
         div.innerHTML = `
-            <a onclick="getDetails('${category.category_id}')" class="tab m-2 bg-[#25252533] active:bg-[#FF1F3D] text-white ac rounded font-medium">${category.category}</a>
+            <a onclick="getDetails('${category.category_id}')" class="tab m-1 md:m-2 bg-[#25252533] active:bg-[#FF1F3D] text-white rounded font-medium">${category.category}</a>
         `
-        clickContainer.appendChild(div)
+        clickContainer.appendChild(div);
     })
 }
 
@@ -39,21 +39,24 @@ const getDetails = async (categories) => {
         nullContainer.appendChild(div)
     }
     wholeData.forEach((data) => {
-        const getSeconds = data.others.posted_date;
-        const hours = parseInt(getSeconds / 3600);
-        const remainingSeconds = getSeconds % 3600;
-        const minutes = parseInt(remainingSeconds / 60);
-        if(hours === 0 && minutes === 0){
-            
+        const getTime = () => {
+            const getSeconds = data.others.posted_date;
+            const hours = parseInt(getSeconds / 3600);
+            const remainingSeconds = getSeconds % 3600;
+            const minutes = parseInt(remainingSeconds / 60);
+            return `${hours}hrs ${minutes}min ago`
         }
-        console.log(hours, minutes)
         const div = document.createElement('div');
        div.innerHTML = ` 
       <img class="w-full md:h-[200px] lg:h-[250px] rounded-lg relative" src="${data?.thumbnail}" alt="Shoes" />
+      
+      
        <div class="flex gap-2 items-center mt-5">
        <img class="w-[40px] rounded-full" src="${data?.authors[0]?.profile_picture}" />
-         <p class="font-bold text-[#171717]"> ${data?.title} </p>
-         <p class="absolute ml-32 mb-32 lg:ml-56 lg:mb-40 bg-[#171717] text-white rounded-lg px-0 py-0 md:px-2 md:py-1"> ${hours? hours : ""}hrs ${minutes? minutes : ""}min ago</p>
+        <p class="font-bold text-[#171717]"> ${data?.title} </p>
+        <p class="bg-[#171717] text-white rounded-lg absolute ml-32 mb-32 lg:ml-56 lg:mb-40">
+        ${data?.others?.posted_date? getTime() : ""}
+        </p>
        </div>
        <div class="flex my-3 gap-2"
        <h3 class="text-[#171717B3] text-sm"> ${data?.authors[0]?.profile_name} </h3>
